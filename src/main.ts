f495518dd55d4aa416554af3d92f766efd2acaec
37o1 <theneonman0@gmail.com>
2025-02-17 09:31:49 +0100

    remove tests

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,6 @@
-import { log } from "console";
+import { clear, log } from "console";
 import PlexServer from "./Server";
 import { randomBytes, UUID } from "crypto";
 import { resolve } from "path";
 
-const password = "ZNlCfjv3whHeNiT0hC3CkrNe06T/CuyUEPsIcyXMio2ps/1AcHsDhmNcQhltNRNkGWHfFJ5Kj+5bs4kcpHuEB/uS/OJ9qrA8bfdUH74TbKfwWG218w7UpWEux/hqUsOQofvXey9JGIAVnF1GlVrWzIxNTDFD+rfrmCLsMnYaIQQ=";
-
-const db = new PlexServer(resolve("db"));
-
-class User {
-	public username: string;
-	public id: UUID;
-}
-
-(async () => {
-	const userCollection = db.collection("user", new User(), {
-		id: {},
-		username: {
-			required: true,
-			default() {
-				return randomBytes(32).toString("base64")
-			},
-			index: true,
-			unique: true,
-		}
-	});
-
-	const user = await userCollection.findOne({
-		username: "UM2WLsIxHnCKnRkp5TRD3RXtyjjdGvSty5Zq4Frp4AU="
-	});
-
-	log(user);
-
-})();
+export default PlexServer;
